5424d88714ba4ca5e13ee19baecb8036bc6bce1d
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-11-10 20:10:50 +0900

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flamingo/flamingo.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flamingo.initializeApp();
   runApp(MyApp());
 }
 
