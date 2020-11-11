8ee1d2a206036ad853e09108fc4c6ed89311d974
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-11-11 18:58:31 +0900

    added test

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -50,17 +50,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
+              key: const Key('counter'),
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: const Key('increment'),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
--- test/domain/repositories/firebase_auth_repository/firebase_auth_repository_test.dart
+++ test/domain/repositories/firebase_auth_repository/firebase_auth_repository_test.dart
@@ -11,6 +11,23 @@ void main() {
     const email = 'mokumoku@moku.com';
     const password = 'password';
 
+    test('[成功] ユーザー情報を取得する', () async {
+      const isEmailVerified = true;
+      final auth = MockFirebaseAuth(signedIn: true);
+      final googleSignIn = MockGoogleSignIn();
+
+      when(auth.currentUser.emailVerified).thenReturn(isEmailVerified);
+
+      final impl = FirebaseAuthRepositoryImpl(auth, googleSignIn);
+
+      expect(impl.loggedInUserId, auth.currentUser.uid);
+      expect(impl.isAnonymously, auth.currentUser.isAnonymous);
+      expect(impl.isEmailVerification, isEmailVerified);
+      expect(await impl.idToken, await auth.currentUser.getIdToken());
+
+      verify(auth.currentUser.emailVerified).called(1);
+    });
+
     test('[成功] サインアップ（メールアドレスとパスワード）', () async {
       final auth = MockFirebaseAuth();
       final googleSignIn = MockGoogleSignIn();
@@ -26,6 +43,7 @@ void main() {
               email: email, password: password))
           .called(1);
     });
+
     test('[成功] サインアップ（他のCredentialとリンク）', () async {
       final auth = MockFirebaseAuth(signedIn: true);
       final googleSignIn = MockGoogleSignIn();
@@ -38,6 +56,34 @@ void main() {
       final result = await impl.linkWithCredential(credential);
       expect(result, isNotNull);
       verify(auth.currentUser.linkWithCredential(credential)).called(1);
+    });
+
+    test('[成功] 国コードと電話番号で認証する', () async {
+      const countryCode = '+81';
+      const phoneNumber = '09000000000';
+      final auth = MockFirebaseAuth(signedIn: true);
+      final googleSignIn = MockGoogleSignIn();
+      when(auth.verifyPhoneNumber(
+        phoneNumber: countryCode + phoneNumber,
+        forceResendingToken: null,
+        verificationCompleted: null,
+        verificationFailed: null,
+        codeSent: null,
+        codeAutoRetrievalTimeout: null,
+      )).thenAnswer((_) => Future<void>.value());
+
+      final impl = FirebaseAuthRepositoryImpl(auth, googleSignIn);
+      await impl.verifyPhoneNumber(countryCode, phoneNumber);
+      verify(await auth.verifyPhoneNumber(
+        phoneNumber: countryCode + phoneNumber,
+        forceResendingToken: null,
+        verificationCompleted: null,
+        verificationFailed: null,
+        codeSent: null,
+        codeAutoRetrievalTimeout: null,
+        timeout: const Duration(seconds: 60),
+      ))
+          .called(1);
     });
   });
 }
